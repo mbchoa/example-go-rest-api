1c6b64fc5a7b8ec3132a3fffd477b19647d2d389
Michael-Bryant Choa <mbchoa@gmail.com>
2020-06-08 23:06:15 -0700

    feat: Add 1-hello-world example

---
+++ 1-hello-world/main.go
@@ -0,0 +1,18 @@
+package helloworld
+
+import (
+	"io"
+	"net/http"
+)
+
+func HandleRoot(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello world!")
+}
+
+func main() {
+	// add request handler for "/" route
+	http.HandleFunc("/", HandleRoot)
+
+	// serve and listen on http://localhost:8080
+	http.ListenAndServe(":8080", nil)
+}
---
+++ 1-hello-world/main_test.go
@@ -0,0 +1,24 @@
+package helloworld
+
+import (
+	"io/ioutil"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandleRoot(t *testing.T) {
+	req := httptest.NewRequest("GET", "http://localhost:8080", nil)
+	w := httptest.NewRecorder()
+	HandleRoot(w, req)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	if respStatusCode := resp.StatusCode; respStatusCode != 200 {
+		t.Errorf("resp.StatusCode == %d, want %d", respStatusCode, 200)
+	}
+
+	if respBody := string(body); respBody != "Hello world!" {
+		t.Errorf("resp.Body == %s, want %s", respBody, "Hello world!")
+	}
+}
