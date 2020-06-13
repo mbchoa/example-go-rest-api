7e8e0268993ded4cdc91d0b11c3fe5954d9ca163
Michael-Bryant Choa <mbchoa@gmail.com>
2020-06-13 15:27:59 -0700

    refactor: Implement remaining handlers for 2-rest-inmemory example

--- 2-rest-inmemory/main.go
+++ 2-rest-inmemory/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -15,35 +16,116 @@ type Book struct {
 }
 
 // Library represents the in-memory database of books
-var Library []Book
+var library = []Book{}
 
 // HandleCreate callback for adding a new book to the library
 func HandleCreate(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint: HandleCreate")
+
+	var newBook Book
+	err := json.NewDecoder(r.Body).Decode(&newBook)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"message": "Unable to add new book to library."})
+		return
+	}
+
+	library = append(library, newBook)
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newBook)
 }
 
 // HandleGetBookByID callback for returning a book from the library
 func HandleGetBookByID(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint: HandleGetBookById")
+
+	params := mux.Vars(r)
+
+	for _, book := range library {
+		if book.ID == params["id"] {
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(&book)
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusNotFound)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Unable to find book."})
 }
 
 // HandleGetAllBooks callback for returning all books from the library
 func HandleGetAllBooks(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint: HandleGetAllBooks")
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(library)
 }
 
 // HandleUpdateByID callback for updating a single book from the library
 func HandleUpdateByID(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint: HandleUpdate")
+
+	id := mux.Vars(r)["id"]
+
+	for idx := range library {
+		book := &library[idx]
+		if book.ID == id {
+			// write updated book fields received from client into book at index
+			err := json.NewDecoder(r.Body).Decode(book)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				json.NewEncoder(w).Encode(map[string]string{"message": "Unable to parse updated book values."})
+				return
+			}
+
+			// send OK response
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(map[string]string{"id": id})
+			return
+		}
+	}
+
+	// book not found, send 409 response
+	// https://stackoverflow.com/questions/10727699/is-http-404-an-appropriate-response-for-a-put-operation-where-some-linked-resour
+	w.WriteHeader(http.StatusConflict)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Unable to update book, selected book not found."})
 }
 
 // HandleDeleteByID callback for deleting a single book from the library
 func HandleDeleteByID(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint: HandleDelete")
+
+	id := mux.Vars(r)["id"]
+
+	for idx := range library {
+		book := &library[idx]
+		if book.ID == id {
+			// re-slice and remove the book element
+			// https://stackoverflow.com/questions/37334119/how-to-delete-an-element-from-a-slice-in-golang
+			library = append(library[:idx], library[idx+1:]...)
+
+			// send OK response
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+	}
+
+	// book to delete not found, send 409 response
+	w.WriteHeader(http.StatusConflict)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Unable to delete book, selected book not found."})
+}
+
+func jsonContentTypeHeaderMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
 }
 
 func main() {
 	r := mux.NewRouter()
+	r.Use(jsonContentTypeHeaderMiddleware)
 
 	// add a new book to library catalog
 	r.HandleFunc("/books", HandleCreate).Methods(http.MethodPost)
